2d1fd530010b4c25dacbf49f5931a8fdb129f2d7
Adrian Marrero <adrianmarrero18@gmail.com>
2021-05-18 15:39:24 -0400

    Nueva migracion de users y factories creados

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,27 @@ class UserFactory extends Factory
     {
         return [
             'name' => 'Adrian',
+            'username' => 'adrian',
+            'type_document' => 'type_document_here',
+            'document_id' => 123456789,
             'email' => 'adrianmarrero18@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+
+            'telephone' => 1526312,
+            'website' => 'website',
+            'instagram' => 'instagram',
+            'facebook' => 'facebook',
+            'twitter' => 'twitter',
+            'snapchat' => 'snapchat',
+            'tiktok' => 'tiktok',
+
+            'address' => 'address',
+            'second_address' => 'second_address',
+            'zip_code' => 123456,
+            'country' => 'country',
+            'state' => 'state',
+
             'is_admin' => 1,
             'remember_token' => Str::random(10),
         ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,34 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
+            // First Page
             $table->string('name');
+            $table->string('username');
+            $table->string('type_document');
+            $table->integer('document_id');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            // Second Page
+            $table->integer('telephone');
+            $table->string('website')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('snapchat')->nullable();
+            $table->string('tiktok')->nullable();
+
+            // Third Page
+            $table->string('address')->nullable();
+            $table->string('second_address')->nullable();
+            $table->integer('zip_code')->nullable();
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+
+            // Developer Control
             $table->boolean('is_admin')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
